a7807cd5435e82097f7a4346568f4cf3bdc01c6b
zakaria <zakariaselassi@gmail.com>
2021-03-12 16:20:47 +0100

    update array

--- assets/PHP/moreArray.php
+++ assets/PHP/moreArray.php
@@ -9,13 +9,16 @@
   array_push($web_development['backend'], 'Ruby');
   array_push($web_development['frontend'], 'CSS');
   array_push($web_development['backend'], 'Flash');
-  array_push($web_development['frontend'], 'Javascript');
+  array_push($web_development['frontend'], 'CSS');
 
-  $updateArr = array( 1 => 'html');
-  print_r(array_replace($web_development['frontend'],$updateArr));
-
+  //$updateArr = array( 1 => 'html');
+  //print_r(array_replace($web_development['frontend'],$updateArr));
+  $search = "CSS";
   print_r(array_splice($web_development['backend'],0,1));
   
+  $search = 'CSS';
+  $found = array_search('CSS', $web_development['frontend']);
+   echo  $found ;
 
   // print_r($web_development);
   // var_dump($web_development);
