a57dc42840435a610e1ccbc9c466c6cdd79e8411
zakaria <zakariaselassi@gmail.com>
2021-03-12 14:11:37 +0100

    update

--- assets/PHP/arrayX1.php
+++ assets/PHP/arrayX1.php
@@ -1,8 +1,22 @@
 <?php 
+$motherArray = array(
+  'age' => 50,
+  'season' => 'winter',
+  'soccer' => false,
+  'hobbies' => array('manger', 'dormir', 'manger')
+  );
 $me = array(
 'age' => 23,
 'season' => 'summer',
-'soccer' => true);
+'soccer' => true,
+'hobbies' => array('manger', 'dormir', 'manger'),
+'mother' => $motherArray
+);
 
-print_r($me);
+
+  echo "<pre>";
+  print_r(count($motherArray['hobbies']) +count($me['hobbies']));
+  echo "<br>";
+  print_r(count($me['hobbies']));
+  echo "</pre>";
 ?>
