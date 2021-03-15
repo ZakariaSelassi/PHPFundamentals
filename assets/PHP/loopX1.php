61ccd107c438566b7c525cf929df370578d107d9
zakaria <zakariaselassi@gmail.com>
2021-03-15 11:36:29 +0100

    update loop finish

--- assets/PHP/loopX1.php
+++ assets/PHP/loopX1.php
@@ -29,4 +29,43 @@ for($i=0; $i<=100; $i++)
 {   
     echo $i;
 }
+
+$nameBe = array("Humberto Mahlum",
+"Domitila Borne",
+"Lakia Pettiway",
+"Zaida Wilcoxen",
+"Caterina Burkley",
+"Isela Mcshan",
+"Arthur Logsdon",
+"Bernie Waxman",
+"Peggie Ridgway",
+"Vernia Mcfalls",
+"Nelia Naron",
+"Lucius Herrod",
+"Stacie Luedtke",
+"Victoria Levins",
+"Reid Mijares",
+"Zona Chagnon",
+"Verla Hardison",
+"Madalyn Remmers",
+"Domingo Kelly",
+"Angel Berney");
+
+foreach($nameBe as $name)
+{
+    echo $name . "<br>";
+}
+
+$country = array('BE' => "Belgique", 'NL' => "Pays-bas",'FR' => "France",'ES' => "Espagne" ,'DE' => "Allemagne", "PT" => "Portugale", "S" => "Suisse", "I" => "Itali");
 ?>
+
+<label for="country-select"> Country </label>
+<select name="country" id="country-select">
+    <option value="">Choose a country</option>
+    <?php
+        foreach($country as $key => $value)
+        {
+            echo '<option value="'.$value.'">'.$key.'</option>';
+        }
+    ?>
+</select>
